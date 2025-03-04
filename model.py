dc8356e35905c07e981d9397f541956f5e4a2128
Krishnanshu Agrawal <krishnanshuagrawal051@gmail.com>
2025-03-04 23:43:39 +0530

    made sample model using tf

--- model.py
+++ model.py
@@ -19,3 +19,25 @@ X_train = X_train / 255
 X_test = X_test / 255        #normalizing the data to be btw 0 and 1
 
 print(X_train[0])
+
+
+
+CnnModel = models.Sequential([
+
+                layers.Conv2D(filters=32 , kernel_size =(3,3) , activation= 'relu' , input_shape= X_train[0].shape),
+                layers.MaxPooling2D((2,2)),
+
+                layers.Conv2D(filters=64 , kernel_size= (3,3) , activation='relu'),
+                layers.MaxPooling2D((2,2)),
+
+                layers.Flatten(),
+                layers.Dense(64 , activation='relu'),
+                layers.Dense(10 , activation= 'softmax')
+
+                ])
+
+CnnModel.compile(optimizer='adam', 
+                  loss = 'sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+CnnModel.fit(X_train , y_train , epochs= 10)   
