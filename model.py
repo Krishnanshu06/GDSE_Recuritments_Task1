23f3105005492d0460436486661700f8a681be29
Krishnanshu Agrawal <krishnanshuagrawal051@gmail.com>
2025-03-05 12:08:27 +0530

    cleaned up code

--- model.py
+++ model.py
@@ -2,35 +2,32 @@ import tensorflow as tf
 from keras import datasets , layers , models
 import numpy as np
 
-(X_train , y_train) , (X_test ,y_test) = datasets.cifar10.load_data()
 
-# print(X_train.shape)
-# print(X_train[0].shape)
-# print(X_train[0])
-# print(y_train[:5])
+#############################################################################################################################
+#initializing and normalizing data
+
+(X_train , y_train) , (X_test ,y_test) = datasets.cifar10.load_data()
 
 outputClasses = ["airplane" , "automobile" , "bird" , "cat" , "deer" , "dog" , "frog" , "horse" , "ship" , "truck"]
 
 y_train = y_train.reshape(-1,) #making it a 1d array
-# print(y_train[:5])
-
-6
 
 X_train = X_train / 255
 X_test = X_test / 255        #normalizing the data to be btw 0 and 1
-
-# print(X_train[0])
 
 
 #############################################################################################################################
 # Hyperparameters
 
-dropoutProb1 = 0.2
-dropoutProb2 = 0.3
-dropoutProb3 = 0.4
-dropoutProb4 = 0.5
-batchSize = 64
+dropoutProb1 = 0.1
+dropoutProb2 = 0.2
+dropoutProb3 = 0.3
+dropoutProb4 = 0.4
+batchSize = 100
 nEpoch = 30
+InitLR = 0.0005
+
+
 
 #############################################################################################################################
 #Model
@@ -49,8 +46,12 @@ CnnModel = models.Sequential([
 
                 layers.Conv2D(filters=128 , kernel_size= (3,3) , activation='relu'),
                 layers.BatchNormalization(),
-                layers.MaxPooling2D((2,2)),
                 layers.Dropout(dropoutProb3),
+
+                layers.Conv2D(filters=256 , kernel_size=(3,3), activation='relu'),
+                layers.BatchNormalization(),
+                layers.MaxPooling2D((2,2)),
+                layers.Dropout(dropoutProb4),
 
                 layers.Flatten(),
                 layers.Dense(256 , activation='relu'),
@@ -62,15 +63,17 @@ CnnModel = models.Sequential([
                 ])
 
 
-CnnModel.compile(optimizer='adam', 
+optimizerAdam = tf.keras.optimizers.Adam(learning_rate = InitLR)
+
+CnnModel.compile(optimizer=optimizerAdam, 
                   loss = 'sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
 
-early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=8, restore_best_weights=True)
 # this is the callback for the early stopping regularization meathod.
 lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', patience=3, factor=0.5, verbose=1)
-
+# reducing lr when the loss is stagnent to converge faster.
 
 
 SaveHistory=CnnModel.fit(X_train , y_train , 
@@ -88,8 +91,8 @@ from sklearn.metrics import classification_report
 predictions = np.argmax(CnnModel.predict(X_test) , axis=1)
 true = y_test.reshape(-1,)
 
-
 print(classification_report(true , predictions, target_names= outputClasses))
+
 
 #############################################################################################################################
 #plotting acc and loss per epoch
@@ -98,14 +101,14 @@ import matplotlib.pyplot as plt
 
 plt.figure(figsize=(12, 5))
 
-# Accuracy plot
+# acc plot
 plt.subplot(1, 2, 1)
 plt.plot(SaveHistory.history['accuracy'], label='Train Accuracy')
 plt.plot(SaveHistory.history['val_accuracy'], label='Val Accuracy')
 plt.legend()
 plt.title('Model Accuracy')
 
-# for Loss plot
+# for loss plot
 plt.subplot(1, 2, 2)
 plt.plot(SaveHistory.history['loss'], label='Train Loss')
 plt.plot(SaveHistory.history['val_loss'], label='Val Loss')
@@ -115,5 +118,5 @@ plt.title('Model Loss')
 plt.show()
 
 
-# even after i tune the hyperparameters, The maximum validation accuracy is about 75%.
+# even after i tune the hyperparameters, The maximum validation accuracy is about 78%.
 # for further improvements we will use a pre trianed model and build our model on it.
